c333f870bd6e47086cc8dd9b517135317c1a4542
nayasis <nayasis@gmail.com>
2021-12-14 17:32:46 +0900

    add serializable interface

--- src/main/kotlin/com/github/nayasis/kotlin/javafx/control/combobox/Item.kt
+++ src/main/kotlin/com/github/nayasis/kotlin/javafx/control/combobox/Item.kt
@@ -2,12 +2,13 @@ package com.github.nayasis.kotlin.javafx.control.combobox
 
 import au.com.console.kassava.kotlinEquals
 import au.com.console.kassava.kotlinHashCode
+import java.io.Serializable
 
 data class Item(
     val value: String?,
     var label: String?,
     var ref: Any? = null
-) {
+): Serializable {
 
     constructor(value: String): this(value,value)
 
