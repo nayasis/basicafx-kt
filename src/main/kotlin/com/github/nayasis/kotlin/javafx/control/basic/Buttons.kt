d15c68abb26c238614a866f9145d6d594ff47072
nayasis <nayasis@gmail.com>
2021-12-27 16:27:00 +0900

    change to constructor

--- src/main/kotlin/com/github/nayasis/kotlin/javafx/control/basic/Buttons.kt
+++ src/main/kotlin/com/github/nayasis/kotlin/javafx/control/basic/Buttons.kt
@@ -4,6 +4,9 @@ import javafx.scene.control.Button
 import javafx.scene.control.OverrunStyle
 import org.controlsfx.glyphfont.FontAwesome
 
-fun Button(glyph: FontAwesome.Glyph, text: String? = null): Button {
-    return Button(text,FontAwesome().create(glyph)).apply { textOverrun = OverrunStyle.CLIP }
+fun Button(text: String? = null, glyph: FontAwesome.Glyph? = null): Button {
+    return when (glyph) {
+        null -> Button(text)
+        else -> Button(text,FontAwesome().create(glyph))
+    }.apply { textOverrun = OverrunStyle.CLIP }
 }
