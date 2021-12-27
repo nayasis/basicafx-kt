3eb0b0d98d9a602c9b6336cc2c34b526e3a832f5
nayasis <nayasis@gmail.com>
2021-12-27 16:24:23 +0900

    change to constructor

--- src/main/kotlin/com/github/nayasis/kotlin/javafx/control/basic/Buttons.kt
+++ src/main/kotlin/com/github/nayasis/kotlin/javafx/control/basic/Buttons.kt
@@ -4,7 +4,7 @@ import javafx.scene.control.Button
 import javafx.scene.control.OverrunStyle
 import org.controlsfx.glyphfont.FontAwesome
 
-fun Button.create(text: String? = null, glyph: FontAwesome.Glyph? = null): Button {
+fun Button(text: String? = null, glyph: FontAwesome.Glyph? = null): Button {
     return when (glyph) {
         null -> Button(text)
         else -> Button(text,FontAwesome().create(glyph))
