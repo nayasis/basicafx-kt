df2847145f8b45be6a29c645d518acb77a15c6cd
nayasis <nayasis@gmail.com>
2022-10-12 17:34:24 +0900

    can handle Pane object.

--- src/main/kotlin/com/github/nayasis/kotlin/javafx/property/SizeProperty.kt
+++ src/main/kotlin/com/github/nayasis/kotlin/javafx/property/SizeProperty.kt
@@ -1,5 +1,6 @@
 package com.github.nayasis.kotlin.javafx.property
 
+import javafx.scene.layout.Pane
 import javafx.stage.Stage
 import java.io.Serializable
 
@@ -9,9 +10,17 @@ data class SizeProperty(
 ): Serializable {
 
     constructor(stage: Stage?): this() { stage?.let { read(it) } }
+    constructor(pane: Pane?): this() { pane?.let { read(it) } }
 
     fun read(stage: Stage?) {
         stage?.let {
+            width  = it.width.toInt()
+            height = it.height.toInt()
+        }
+    }
+
+    fun read(pane: Pane?) {
+        pane?.let {
             width  = it.width.toInt()
             height = it.height.toInt()
         }
@@ -21,6 +30,13 @@ data class SizeProperty(
         stage?.let {
             it.width  = width.toDouble()
             it.height = height.toDouble()
+        }
+    }
+
+    fun bind(pane: Pane?) {
+        pane?.let {
+            it.prefWidth  = width.toDouble()
+            it.prefHeight = height.toDouble()
         }
     }
 
