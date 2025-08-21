c67a560854f87ed2a17ee0cfd939d0aa1edf5374
nayasis <nayasis@gmail.com>
2025-08-21 17:55:20 +0900

    enhance TerminalFxSample: add title label and update text upon command completion

--- src/test/kotlin/io/github/nayasis/kotlin/javafx/terminal/TerminalFxSample.kt
+++ src/test/kotlin/io/github/nayasis/kotlin/javafx/terminal/TerminalFxSample.kt
@@ -3,11 +3,11 @@ package io.github.nayasis.kotlin.javafx.terminal
 import com.pty4j.PtyProcessBuilder
 import com.techsenger.jeditermfx.app.pty.PtyProcessTtyConnector
 import com.techsenger.jeditermfx.core.TerminalColor
-import com.techsenger.jeditermfx.core.TtyConnector
 import com.techsenger.jeditermfx.ui.DefaultHyperlinkFilter
 import com.techsenger.jeditermfx.ui.JediTermFxWidget
 import com.techsenger.jeditermfx.ui.settings.DefaultSettingsProvider
 import io.github.oshai.kotlinlogging.KotlinLogging
+import javafx.scene.control.Label
 import javafx.stage.Stage
 import tornadofx.*
 import java.nio.charset.StandardCharsets
@@ -34,12 +34,19 @@ class TerminalFxSample: App(TerminalFxSampleView::class) {
 
 class TerminalFxSampleView : View("TerminalFx Sample") {
 
+    private lateinit var titleLabel: Label
+
     override val root = vbox {
-        label("This is a sample terminal application using JavaFX.")
+        titleLabel = label("This is a sample terminal application using JavaFX.")
         button("Click Me") {
             action {
                 val widget = createTerminal()
                 widget.pane.attachTo(this@vbox)
+
+                widget.ttyConnector.waitFor()
+
+                // 명령어 실행 완료 후 제목에 "done" 표시
+                titleLabel.text = "This is a sample terminal application using JavaFX. - done"
             }
         }
     }
@@ -50,9 +57,6 @@ private fun createTerminal(): JediTermFxWidget {
     return JediTermFxWidget(80, 24, DarkThemeSettingsProvider()).apply {
         ttyConnector = createTtyConnector()
         addHyperlinkFilter(DefaultHyperlinkFilter())
-        addListener {
-            ttyConnector.close()
-        }
         start()
     }
 }
@@ -67,14 +71,13 @@ class DarkThemeSettingsProvider : DefaultSettingsProvider() {
     }
 }
 
-private fun createTtyConnector(): TtyConnector {
+private fun createTtyConnector(): PtyProcessTtyConnector {
     try {
-        val command = listOf("dir")
+        val command = listOf("ls", "-al")
         val envs = HashMap<String,String>(System.getenv()).apply {
             put("TERM", "xterm-256color")
         }
         val process = PtyProcessBuilder().setCommand(command.toTypedArray()).setEnvironment(envs).start()
-        process.waitFor()
         return PtyProcessTtyConnector(process, StandardCharsets.UTF_8)
     } catch (e: Exception) {
         throw IllegalStateException(e)
