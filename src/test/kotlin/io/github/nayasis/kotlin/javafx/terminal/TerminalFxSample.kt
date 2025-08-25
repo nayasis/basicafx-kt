cf40901f19155abec97e670a448bb9f956e165a7
nayasis <nayasis@gmail.com>
2025-08-25 15:27:00 +0900

    set black theme

--- src/test/kotlin/io/github/nayasis/kotlin/javafx/terminal/TerminalFxSample.kt
+++ src/test/kotlin/io/github/nayasis/kotlin/javafx/terminal/TerminalFxSample.kt
@@ -1,19 +1,18 @@
 package io.github.nayasis.kotlin.javafx.terminal
 
-import com.pty4j.PtyProcessBuilder
-import com.techsenger.jeditermfx.app.pty.PtyProcessTtyConnector
-import com.techsenger.jeditermfx.core.TerminalColor
-import com.techsenger.jeditermfx.ui.DefaultHyperlinkFilter
-import com.techsenger.jeditermfx.ui.JediTermFxWidget
-import com.techsenger.jeditermfx.ui.settings.DefaultSettingsProvider
-import io.github.oshai.kotlinlogging.KotlinLogging
-import javafx.stage.Stage
-import tornadofx.*
-import java.nio.charset.StandardCharsets
-import org.slf4j.LoggerFactory
 import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.Logger
+import com.pty4j.PtyProcessBuilder
+import com.techsenger.jeditermfx.app.pty.PtyProcessTtyConnector
+import com.techsenger.jeditermfx.ui.DefaultHyperlinkFilter
+import com.techsenger.jeditermfx.ui.JediTermFxWidget
+import io.github.nayasis.kotlin.javafx.terminal.theme.BlackTerminalTheme
+import io.github.oshai.kotlinlogging.KotlinLogging
 import javafx.scene.layout.Pane
+import javafx.stage.Stage
+import org.slf4j.LoggerFactory
+import tornadofx.*
+import java.nio.charset.StandardCharsets
 
 private val logger = KotlinLogging.logger {}
 
@@ -24,11 +23,10 @@ fun main(vararg args: String) {
 
 private fun configureLogging() {
     listOf(
-        "com.techsenger.jeditermfx.core.emulator.JediEmulator",
         "com.techsenger.jeditermfx.core",
         "com.techsenger.jeditermfx.ui",
         "com.techsenger.jeditermfx",
-        "com.pty4j"
+        "com.pty4j",
     ).forEach { packageName ->
         (LoggerFactory.getLogger(packageName) as Logger).level = Level.WARN
     }
@@ -39,8 +37,8 @@ class TerminalFxSample: App(TerminalFxSampleView::class) {
         super.start(stage)
         // set window size for better terminal display with Korean text
         stage.apply {
-            width     = 400.0
-            height    = 250.0
+            width     = 450.0
+            height    = 400.0
             minWidth  = 100.0
             minHeight =  40.0
         }
@@ -58,6 +56,9 @@ class TerminalFxSampleView : View("JediTermFx Sample") {
         }
         terminalWidget.pane.attachTo(this)
         runLater {
+            runAsync{
+                terminalWidget.ttyConnector.write("dir\r")
+            }
             runAsync{
                 terminalWidget.ttyConnector.waitFor()
                 runLater {
@@ -84,27 +85,17 @@ class TerminalFxSampleView : View("JediTermFx Sample") {
 }
 
 private fun createTerminal(): JediTermFxWidget {
-    return JediTermFxWidget(80, 200, CustomSettingsProvider()).apply {  // 더 넓은 터미널
+    return JediTermFxWidget(80, 200, BlackTerminalTheme()).apply {  // 더 넓은 터미널
         ttyConnector = createTtyConnector()
         addHyperlinkFilter(DefaultHyperlinkFilter())
         start()
     }
 }
 
-class CustomSettingsProvider : DefaultSettingsProvider() {
-    override fun getTerminalFontSize(): Float {
-        return 10.2f
-    }
-
-    override fun getLineSpacing(): Float {
-        return 1.2f
-    }
-}
-
 private fun createTtyConnector(): PtyProcessTtyConnector {
     // Windows command example
     val command = listOf("cmd")
-//    val command = listOf("src/test/resources/test-program/test.exe", "100")
+//    val command = listOf("src/test/resources/test-program/test.exe", "5")
     val envs = System.getenv().toMutableMap().apply {
         put("TERM", "xterm-256color")
     }
